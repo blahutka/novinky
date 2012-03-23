e72915ff3ae3b217c6d8a0f11325367f3b9493e6
blahutka <blahutka@centrum.cz>
2012-03-23 15:48:59 +0100

    will paginate set original

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 ENV['RAILS_ENV'] ||= 'production'
 
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -10,9 +10,9 @@ Rails::Initializer.run do |config|
              :version => '~> 2.1.0'
   config.gem 'haml',
              :version => '~> 2.0.9'
-  config.gem "will_paginate", :source => "http://gems.github.com"
+  config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
   config.gem 'mime-types', :lib => 'mime/types'
-
+ 
   config.active_record.default_timezone = 'Prague'
   
   config.i18n.load_path = Dir[File.join(RAILS_ROOT, 'config', 'locales', '*.{rb,yml}')]
@@ -25,14 +25,6 @@ Rails::Initializer.run do |config|
   config.action_controller.cache_store = :file_store, "/tmp/cache"
   config.action_controller.session_store = :active_record_store
 end
-
-require 'will_paginate'
-require "pry"
-Pry.start
-
-$KCODE = 'u' #$KCODE = 'UTF8'
-require 'jcode'
-
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default_charset = "utf-8"
 ActionMailer::Base.raise_delivery_errors = true
